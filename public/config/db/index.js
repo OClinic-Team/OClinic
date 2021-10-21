5ac40eca183226036b30632547bf7cd1d16c2c05
mrthanhtay <taythanh1999@gmail.com>
2021-10-21 19:56:36 +0700

    fix data mongoDB

--- public/config/db/index.js
+++ public/config/db/index.js
@@ -2,22 +2,22 @@ const mongoose = require('mongoose');
 //database local
 //const MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 //database cloud
-//const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/online-clinic?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/test?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 async function connect() {
-  // const moongoURI ='mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/test?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
- const moongo ='mongodb://localhost:27017/online_clinic'
-  
-  try {
-    await mongoose.connect(moongo, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
-    console.log('ket noi thanh cong');
-  } catch (error) {
-    console.log('Loi!!!!');
-  }
+    // const moongoURI ='mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/test?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+    // const moongo = 'mongodb://localhost:27017/online_clinic'
+
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        });
+        console.log('ket noi thanh cong');
+    } catch (error) {
+        console.log('Loi!!!!');
+    }
 }
 
 module.exports = { connect };
