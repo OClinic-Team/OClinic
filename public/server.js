b439fe7d80771f1243780f6e500574092b234465
mrthanhtay <taythanh1999@gmail.com>
2021-11-19 05:56:34 +0700

    videoscall v5

--- public/server.js
+++ public/server.js
@@ -67,8 +67,43 @@ app.use(passport.session());
 app.use(SortMiddleware);
 app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/fail' }),
-    checkAccount
-);
+    //checkAccount
+    function(req, res, next) {
+        account_patient.findOne({ Id: req.user.id }, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                var check_firstTime = false;
+                if (data === null) {
+                    check_firstTime = true;
+                    data = new account_patient({
+                        Id: req.user.id,
+                        Name: req.user.displayName,
+                        ImageURL: req.user.photos[0].value,
+                        Sex: '',
+                        Adress: '',
+                        Email: req.user.emails[0].value,
+
+                    })
+                    data.save();
+                }
+                //req.cookieSession.         
+                req.session.isAuthenticated = true;
+                req.session.authUser = data;
+                req.session.token = req.user.token;
+                // set data cho biến Local. dùng cho hdb
+                res.locals.lcIsAuthenticated = req.session.isAuthenticated;
+                res.locals.lcAuthUser = req.session.authUser;
+                //dang nhap thanh cong chuyen ve home
+                if (check_firstTime) {
+                    res.redirect(`/profile/${req.user.id}`);
+                } else {
+                    res.redirect('/');
+                }
+            }
+        });
+    });
+
 //set data cho res.locals su dung cho .hdb
 app.use(async function(req, res, next) {
         if (req.session.isAuthenticated === null) {
