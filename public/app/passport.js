1266d346cc43d21c508cc80055f49d19800d0bf3
mrthanhtay <taythanh1999@gmail.com>
2021-12-10 11:01:54 +0700

    deployed web v2

--- public/app/passport.js
+++ public/app/passport.js
@@ -16,7 +16,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
         clientID: '1044360178044-m8dmf3ou6d0vdb9cuaiodudfn67ce5f5.apps.googleusercontent.com',
         clientSecret: 'LKtBVKiujlp-Tbar-UawBdxK',
-        callbackURL: "http://oonlineclinic.herokuapp.com/google/callback"
+        callbackURL: "https://oonlineclinic.herokuapp.com/google/callback"
     },
     function(accessToken, refreshToken, profile, done) {
         return done(null, profile);
