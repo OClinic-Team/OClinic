be2f2d4bfc2924c52378f4e6385b7e484b35c0cc
ntluan111ntb2 <ntluan1.fb@gmail.com>
2021-12-13 22:49:43 +0700

    luan sua UI

--- public/config/db/index.js
+++ public/config/db/index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 //database local
-//const MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
+// const MONGO_URI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false';
 //database cloud
 //const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/test?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 async function connect() {
-    //const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/Online-Clinic'
-        const MONGO_URI = 'mongodb://localhost:27017/online_clinic'
+    const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/Online-Clinic'
+        // const MONGO_URI = 'mongodb://localhost:27017/online_clinic'
 
     try {
         await mongoose.connect(MONGO_URI, {
