4aeeadd68d19679b202ca67f5963102a6bdbce06
mrthanhtay <taythanh1999@gmail.com>
2022-04-20 18:17:42 +0700

    update code cho Khang

--- public/app/middlewares/checkDoctor.js
+++ public/app/middlewares/checkDoctor.js
@@ -1,6 +1,7 @@
 module.exports = function(req, res, next) {
     if (!req.session.authUser.Permission === '1') {
         return res.send('Not your permission');
+    } else {
+        next();
     }
-    next();
 }
