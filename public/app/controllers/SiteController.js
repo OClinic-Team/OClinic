85fdc99bdd012ba852bca44db4d7d4091097f5fd
doanvankhang <62994103+doanvankhang@users.noreply.github.com>
2022-05-13 23:58:21 +0700

    khang ta.

--- public/app/controllers/SiteController.js
+++ public/app/controllers/SiteController.js
@@ -1,15 +1,24 @@
 const Account = require('../models/Account');
 const { mutileMongooseToObject } = require('../../util/mongoose');
+const Blog = require('../models/blog')
+
 class SiteController {
     //Trang Chu
     home(req, res, next) {
         var tmp = new Date();
         tmp.setDate(tmp.getDate() - 1);
         console.log(tmp)
-        Account.find({})
-            .then((accounts) => {
+        // Account.find({})
+        //     .then((accounts) => {
+        //         res.render('home', {
+        //             accounts: mutileMongooseToObject(accounts),
+        //         });
+        //     })
+        //     .catch(next);
+        Blog.find({})
+            .then((blogs) => {
                 res.render('home', {
-                    accounts: mutileMongooseToObject(accounts),
+                    blogs: mutileMongooseToObject(blogs),
                 });
             })
             .catch(next);
@@ -17,37 +26,37 @@ class SiteController {
 
     //[GET] /search
     search(req, res, next) {
-            // res.json(req.query.Khoa);
-            Account.find({ khoa: req.query.Khoa })
+        // res.json(req.query.Khoa);
+        Account.find({ khoa: req.query.Khoa })
 
             .then((accounts) => {
-                    res.render('bacsitheokhoa', {
-                        accounts: mutileMongooseToObject(accounts),
-                    });
-                })
-                .catch(next);
-            //
-            // var khoa = req.query.Khoa
-            // var data = Account.filter(function(item){
-            //     return item.Khoa === khoa;
-            // });
-            // res.render('bacsitheokhoa',{
-            //     accounts: data
-            // });
-            //     Account.find({ khoa: req.query.khoa })
-            //         .then((account) => {
-            //             res.render('accounts/show', {
-            //             account: mutileMongooseToObject(account),
-            //         });
-            //     })
-            //   .catch(next);
-        }
-        // logout(req, res) {
-        //     req.session = null;
-        //     req.logout();
-        //     //logout --> home
-        //     res.redirect('/');
-        // }
+                res.render('bacsitheokhoa', {
+                    accounts: mutileMongooseToObject(accounts),
+                });
+            })
+            .catch(next);
+        //
+        // var khoa = req.query.Khoa
+        // var data = Account.filter(function(item){
+        //     return item.Khoa === khoa;
+        // });
+        // res.render('bacsitheokhoa',{
+        //     accounts: data
+        // });
+        //     Account.find({ khoa: req.query.khoa })
+        //         .then((account) => {
+        //             res.render('accounts/show', {
+        //             account: mutileMongooseToObject(account),
+        //         });
+        //     })
+        //   .catch(next);
+    }
+    // logout(req, res) {
+    //     req.session = null;
+    //     req.logout();
+    //     //logout --> home
+    //     res.redirect('/');
+    // }
 
 }
 
--- public/config/db/index.js
+++ public/config/db/index.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 //database cloud
 //const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/test?authSource=admin&replicaSet=atlas-10rvle-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
 async function connect() {
-    // const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/Online-Clinic'
-    const MONGO_URI = 'mongodb://localhost:27017/online_clinic'
+    const MONGO_URI = 'mongodb+srv://online-clinic-project:123321123321@cluster.mjikz.mongodb.net/Online-Clinic'
+    // const MONGO_URI = 'mongodb://localhost:27017/online_clinic'
     try {
         await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
