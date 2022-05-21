e9f6915f1292667b8eeb1325927547963c10edee
ntluan111ntb2 <ntluan1.fb@gmail.com>
2022-05-21 22:12:40 +0700

    hello world

--- public/routes/blog.js
+++ public/routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const blog = require('../app/controllers/BlogController')
+const blog = require('../app/controllers/blogController')
 const auth = require('../app/middlewares/auth');
 const checkAdmin = require('../app/middlewares/checkAdmin');
 
